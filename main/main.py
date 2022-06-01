d93c047efc4a0a064a4ee3b51fd4c47aa69b8f66
Nikita Smolkin <Nsabest@yandex.ru>
2022-06-01 11:39:27 +0300

    update 0.01
    non-work
    base rework

--- main/main.py
+++ main/main.py
@@ -16,18 +16,44 @@ Button_Text = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'X', '+', '-', 
 class MainApp(App):
     global Button_Text
 
-    def build(self):
+    def __init__(self, **kwargs):
+        super().__init__()
         self.label1 = Label(text='0', font_size=50, halign='right', valign='center',
                             size_hint=(1, .4),
                             text_size=(400 - 20, 500 * 0.4 - 20))
+        self.formula = ''
+
+    def build(self):
         bl = BoxLayout(orientation='vertical', padding=10)
         gl = GridLayout(cols=4, spacing=3, size_hint=(1, .6))
         bl.add_widget(self.label1)
-        # gl.add_widget(Widget())
-        for widget_text in Button_Text:
-            gl.add_widget(Button(text=widget_text))
+
+        gl.add_widget(Button(text='7', on_press=self.add_numb))
+        gl.add_widget(Button(text='8', on_press=self.add_numb))
+        gl.add_widget(Button(text='9', on_press=self.add_numb))
+        gl.add_widget(Button(text='x'))
+
+        gl.add_widget(Button(text='4', on_press=self.add_numb))
+        gl.add_widget(Button(text='5', on_press=self.add_numb))
+        gl.add_widget(Button(text='6', on_press=self.add_numb))
+        gl.add_widget(Button(text='-'))
+
+        gl.add_widget(Button(text='1', on_press=self.add_numb))
+        gl.add_widget(Button(text='2', on_press=self.add_numb))
+        gl.add_widget(Button(text='3', on_press=self.add_numb))
+        gl.add_widget(Button(text='+'))
+
+        gl.add_widget(Widget())
+        gl.add_widget(Button(text='0', on_press=self.add_numb))
+        gl.add_widget(Button(text='.'))
+        gl.add_widget(Button(text='='))
+
         bl.add_widget(gl)
         return bl
+
+    def add_numb(self, instance):
+
+        self.formula += str(instance.text)
 
 
 if __name__ == '__main__':
